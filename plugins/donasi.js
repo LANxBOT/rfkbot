d2174243bcd64e1f2886712f6213bc4f4838a94c
LANxBOT <98082226+LANxBOT@users.noreply.github.com>
2022-01-23 14:39:55 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
-┌〔 Donasi • Emoney 〕
-├ PULSA : 083822759168
-├ OVO & DANA : 083822759168
+┌〔 Donasi • Topup 〕
+├ Digi🥀 : 60149431385
+├ Umobile🥀 : 60189830350
 └────
 `.trim(), footer, 'Owner', '.owner')
 handler.help = ['donasi']
